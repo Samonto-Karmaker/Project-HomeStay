78add450bd237219a38bbe6b2c31db125aced0df
Samonto Karmaker <samonto0726@gmail.com>
2023-11-23 23:04:55 +0600

    Added validator for registration

---
+++ server/middlewares/auth/register_validator.js
@@ -0,0 +1,46 @@
+//External Imports
+const {check, validationResult} = require('express-validator');
+const createError = require('http-errors');
+
+//Internal Imports
+const Actors = require('../../models/Actors');
+
+//Register validator
+const registerValidator = [
+    check('name')
+        .isLength({min: 1})
+        .withMessage('NameError: Name is required')
+        .matches(/^[a-zA-Z ][a-zA-Z\s-_]+$/)
+        .withMessage("NameError: Name must start with an alphabet and should only contain alphabets, whitespace, - and _")
+        .trim(),
+    check('email')
+        .isEmail()
+        .withMessage('EmailError: Please enter a valid email address')
+        .trim()
+        .custom(async (value) => {
+            try {
+                const user = await Actors.findOne({email: value});
+                if (user) {
+                    throw createError('EmailError: Email already in use');
+                }
+            } catch (error) {
+                throw createError(error.message);
+            }
+        }),
+    check('mobile')
+        .isMobilePhone()
+        .withMessage('MobileError: Please enter a valid mobile number')
+        .custom(async (value) => {
+            try {
+                const user = await Actors.findOne({mobile: value});
+                if (user) {
+                    throw createError('MobileError: Mobile number already in use');
+                }
+            } catch (error) {
+                throw createError(error.message);
+            }
+        }),
+    check('password')
+        .isStrongPassword()
+        .withMessage('PasswordError: Password must be at least 8 characters long and should contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol'),
+]
