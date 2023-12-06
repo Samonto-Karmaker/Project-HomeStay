9a8cb29a182b38b123a23dfa446a95e8779a91c9
Samonto Karmaker <samonto0726@gmail.com>
2023-12-06 22:30:12 +0600

    Added login functionality in controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 //External imports
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //Internal imports
 const Actors = require('../models/Actors');
@@ -51,6 +52,65 @@ const register = async (req, res, next) => {
     }
 }
 
+//Login user
+const login = async (req, res, next) => {
+    try{
+        const user = await Actors.findOne({
+            $or: [
+                { email: req.body.email }, 
+                { mobile: req.body.mobile }
+            ]
+        })
+        if(user && user._id){
+            const isValidPassword = await bcrypt.compare(
+                req.body.password, 
+                user.password
+            );
+            if(isValidPassword) {
+                const userObject = {
+                    userId: user._id,
+                    name: user.name,
+                    email: user.email,
+                    mobile: user.mobile,
+                    isOwner: user.isOwner,
+                }
+                const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRY
+                });
+                res.cookie(process.env.COOKIE_NAME, token, {
+                    maxAge: process.env.JWT_EXPIRY,
+                    httpOnly: true,
+                    signed: true
+                });
+                res.status(200).json({
+                    success: true,
+                    message: 'User logged in successfully',
+                    loggedInUser: userObject,
+                });
+            }
+            else{
+                res.status(401).json({
+                    success: false,
+                    message: 'Invalid Password',
+                });
+            }
+        }
+        else{
+            res.status(401).json({
+                success: false,
+                message: 'Invalid Credentials',
+            });
+        }
+    }
+    catch(error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+}
+
 module.exports = {
     register,
+    login,
 }
