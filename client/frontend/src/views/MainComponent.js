571665752f51cf40ac4a4cdab275ccdc31477180
Samonto Karmaker <samonto0726@gmail.com>
2023-12-08 21:56:32 +0600

    Check auth in Main Component: persist auth status

--- client/frontend/src/views/MainComponent.js
+++ client/frontend/src/views/MainComponent.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useEffect, useContext} from "react";
+import { UserContext } from "../components/context/UserContext";
 
 import Header from "./header/Header";
 import Body from "./body/Body";
 import Footer from "./footer/Footer";
 
 const MainComponent = () => {
+
+  const {setUser} = useContext(UserContext);
+
+  const fetchUserInfo = async () => {
+    try{
+      const response = await fetch('/api/check-authentication', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      if(result.success){
+        setUser(result.loggedInUser);
+      }
+      else{
+        setUser(null);
+        window.alert(result.message);
+      }
+    }
+    catch(err){
+      window.alert("Something went wrong");
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchUserInfo()
+  }, [])
+
   return (
     <div className="main-component">
       <Header />
