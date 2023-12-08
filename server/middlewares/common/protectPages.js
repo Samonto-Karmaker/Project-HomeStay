ba56ac56a08db36797660ea6ac71ddc6efa32b9f
Samonto Karmaker <samonto0726@gmail.com>
2023-12-08 21:05:49 +0600

    Added a check auth middleware

---
+++ server/middlewares/common/protectPages.js
@@ -0,0 +1,24 @@
+//External imports
+const jwt = require('jsonwebtoken');
+
+const checkAuth = (req, res, next) => {
+    let cookies = Object.keys(req.signedCookies).length === 0 ? null : req.signedCookies;
+    if(cookies) {
+        try{
+            token = cookies[process.env.COOKIE_NAME];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
+            next();
+        }
+        catch(err){
+            res.status(401).json({
+                success: false,
+                message: 'Unauthorized access',
+            });
+        }
+    }
+}
+
+module.exports = {
+    checkAuth
+}
