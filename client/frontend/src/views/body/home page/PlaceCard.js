462b238f06ae2b2aa050c30cbb69bceb10fab820
Samonto Karmaker <samonto0726@gmail.com>
2024-01-31 22:36:38 +0600

    Designed PlaceCard component UI

--- client/frontend/src/views/body/home page/PlaceCard.js
+++ client/frontend/src/views/body/home page/PlaceCard.js
@@ -1,15 +1,40 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import StarRating from 'react-star-ratings';
 
 const PlaceCard = ({ place }) => {
   return (
-    <div className="place" key={place._id}>
-      <div className="place-image">
-        <img src={place.images[0]} alt={place.name} />
-      </div>
-      <div className="place-name">{place.name}</div>
-      <div className="place-location">{place.location}</div>
-      <div className="place-description">{place.description}</div>
-      <div className="place-price">{place.price}</div>
+    <div key={place._id}>
+      <Card className="placeCard" 
+        style={{ 
+          width: '20rem', 
+          margin: '15px', 
+          textAlign: "left", 
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)" 
+        }}
+      >
+      <Card.Img variant="top" src={place.images[0]} />
+      <Card.Body>
+        <Card.Title>{place.name}</Card.Title>
+        <StarRating 
+          rating={place.rating}
+          starRatedColor="gold"
+          numberOfStars={5}
+          name="rating"
+          starDimension="20px"
+          starSpacing="2px" 
+        />
+        <Card.Text style={{color: "gray"}}>
+          {place.description}
+        </Card.Text>
+        <Card.Text>
+          {place.city}, {place.country}
+        </Card.Text>
+        <Card.Text style={{color: "red"}}>
+          price: ${place.price} / night
+        </Card.Text>
+      </Card.Body>
+    </Card>
     </div>
   );
 };
