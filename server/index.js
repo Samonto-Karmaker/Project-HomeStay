3e0e9f71c85badb4f66287376f1730555cf2d1ce
Samonto Karmaker <samonto0726@gmail.com>
2024-02-05 21:25:18 +0600

    Add CORS support to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 //Internal Imports
 const authRouter = require('./routes/authRouter');
@@ -37,10 +38,8 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api", authRouter)
 app.use("/api/places", placeRouter)
 
-//Error Handler
-//Default Error Handler
-
-//404: Not Found
+//Enable all CORS requests
+app.use(cors())
 
 //Server listening
 app.listen(process.env.PORT, () => {
