37fae88e816c9ad15c8acbd2155295fcc6ef81d7
Samonto Karmaker <samonto0726@gmail.com>
2024-02-25 21:41:58 +0600

    Add getPlaceByID route to placeRouter.js

--- server/routes/placeRouter.js
+++ server/routes/placeRouter.js
@@ -2,12 +2,15 @@
 const express = require('express');
 
 //Internal imports
-const { getAllPlaces } = require('../controllers/placeController');
+const { getAllPlaces, getPlaceByID } = require('../controllers/placeController');
 
 //Initializing Router
 const router = express.Router();
 
 //Get all places
 router.get('/', getAllPlaces);
+
+//Get a single place by ID
+router.get('/:placeId', getPlaceByID);
 
 module.exports = router;
