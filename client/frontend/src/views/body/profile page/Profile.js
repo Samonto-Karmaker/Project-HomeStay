098e72b91ec0e733b694503f6c1a15774f0ef4af
Samanta Karmaker <samonto0726@gmail.com>
2024-03-18 22:27:34 +0600

    Initial UserInfo component done

--- client/frontend/src/views/body/profile page/Profile.js
+++ client/frontend/src/views/body/profile page/Profile.js
@@ -2,11 +2,23 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import OwnerDashBoard from "./OwnerDashBoard";
 import UserDashBoard from "./UserDashBoard";
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../../../components/context/UserContext";
 
 const Profile = () => {
+
+    const { User } = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if(User) setIsLoading(false);
+    }, [User])
+    
+    if(isLoading) return <div>...Loading</div>
+
     return (
         <div>
-            <UserInfo />
+            <UserInfo user = {User}/>
             <OwnerDashBoard />
             <UserDashBoard />
         </div>
--- client/frontend/src/views/body/profile page/UserInfo.js
+++ client/frontend/src/views/body/profile page/UserInfo.js
@@ -1,10 +1,53 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-const UserInfo = () => {
+const UserInfo = ({user}) => {
     return (
-        <div>
-            <h1>UserInfo</h1>
-        </div>
+        <Card
+            bg="light"
+            text="dark"
+            style={{ 
+                width: "25rem",
+                margin: "35px auto",
+                padding: "15px",
+                borderRadius: "20px",
+                border: "2px solid #f8f9fa",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+            }}
+            className="mb-2"
+        >
+            <Card.Header style={{
+                fontSize: "30px",
+                color: "red",
+                borderTopLeftRadius: "20px",
+                borderTopRightRadius: "20px",
+                borderBottom: "2px solid #f8f9fa",
+            }}
+            >
+                Profile
+            </Card.Header>
+            <Card.Body>
+                <Card.Title
+                    style={{
+                        fontSize: "25px",
+                        fontFamily: "Fantasy",
+                        paddingBottom: "10px",
+                        backgroundColor: "white !important",
+                    }}
+                >
+                    {user.name}
+                </Card.Title>
+                <Card.Text>
+                    Email: {user.email}
+                </Card.Text>
+                <Card.Text>
+                    Mobile: {user.mobile}
+                </Card.Text>
+                <Card.Text>
+                    Is Owner: {user.isOwner ? "Yes" : "No"}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
