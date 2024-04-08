5166cbd19be68353e2b2c9e7c9431356ff4a85b3
Samanta Karmaker <samonto0726@gmail.com>
2024-04-08 22:23:50 +0600

    Added add place endpoint

--- server/routes/placeRouter.js
+++ server/routes/placeRouter.js
@@ -1,32 +1,48 @@
 //External imports
-const express = require('express');
+const express = require("express");
 
 //Internal imports
-const { 
-    getAllPlaces, 
-    getPlaceByID, 
-    getOwnerByID, 
+const {
+    getAllPlaces,
+    getPlaceByID,
+    getOwnerByID,
     updatePlaceAvailability,
-    getPlacesByOwnerID, 
-} = require('../controllers/placeController');
-const { checkAuth } = require('../middlewares/common/protectPages');
+    getPlacesByOwnerID,
+    addPlace,
+} = require("../controllers/placeController");
+const { checkAuth } = require("../middlewares/common/protectPages");
+const {
+    addPlaceValidators,
+    addPlaceValidationHandler,
+} = require("../middlewares/place/add_place_validator");
+const placeImgUploader = require("../middlewares/place/placeImgUploader");
 
 //Initializing Router
 const router = express.Router();
 
 //Get all places
-router.get('/', getAllPlaces);
+router.get("/", getAllPlaces);
 
 //Get a single place by ID
-router.get('/:placeId', getPlaceByID);
+router.get("/:placeId", getPlaceByID);
 
 //Get the owner of a place
-router.get('/owner-info/:ownerId', getOwnerByID);
+router.get("/owner-info/:ownerId", getOwnerByID);
 
 //Update isAvailable field of a place
-router.put('/update-availability/:placeId', checkAuth, updatePlaceAvailability);
+router.put("/update-availability/:placeId", checkAuth, updatePlaceAvailability);
 
 //Get places by owner ID
-router.get('/owner-places/:ownerId', getPlacesByOwnerID);
+router.get("/owner-places/:ownerId", getPlacesByOwnerID);
+
+//Add a new place
+router.post(
+    "/add",
+    checkAuth,
+    addPlaceValidators,
+    addPlaceValidationHandler,
+    placeImgUploader,
+    addPlace
+);
 
 module.exports = router;
