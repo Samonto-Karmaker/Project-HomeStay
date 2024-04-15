9bad765c47a8ae97535d77854116c384f672ec03
Samanta Karmaker <samonto0726@gmail.com>
2024-04-15 21:50:31 +0600

    added validation handler for search places

--- server/middlewares/place/search_place_validator.js
+++ server/middlewares/place/search_place_validator.js
@@ -48,6 +48,19 @@ const searchPlaceValidator = [
         })
 ]
 
+const searchPlaceValidationHandler = (req, res, next) => {
+    const errors = validationResult(req);
+    const mappedErrors = errors.mapped();
+
+    if(Object.keys(mappedErrors).length === 0) next();
+    else {
+        res.status(400).json({
+            errors: mappedErrors
+        });
+    }
+}
+
 module.exports = {
     searchPlaceValidator,
+    searchPlaceValidationHandler
 };
