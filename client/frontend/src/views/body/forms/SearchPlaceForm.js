6cd6d29bfc7b7e2872160107d123ea75b99e8eed
Samanta Karmaker <samonto0726@gmail.com>
2024-04-17 21:55:48 +0600

    Added state management using useReducer in search place form

--- client/frontend/src/views/body/forms/SearchPlaceForm.js
+++ client/frontend/src/views/body/forms/SearchPlaceForm.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Form, Row, Col } from "react-bootstrap";
 import Select from "react-select";
 import commonAmenities from "../../../utilities/commonAmenities";
 import RegularBtn from "../../../components/reusable/RegularBtn";
 
+const ACTIONS = {
+    SET_VALUE: "set-value",
+};
+
+const initialState = {
+    country: "",
+    city: "",
+    minPrice: 0,
+    maxPrice: 0,
+    minCapacity: 0,
+    minRating: 0,
+    amenities: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ACTIONS.SET_VALUE: {
+            return { ...state, [action.field]: action.payload };
+        }
+        default:
+            return state;
+    }
+};
+
 const SearchPlaceForm = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const {
+        country,
+        city,
+        minPrice,
+        maxPrice,
+        minCapacity,
+        minRating,
+        amenities,
+    } = state;
+
     const amenitiesOptions = commonAmenities.map((amenity) => {
         return { value: amenity, label: amenity };
     });
 
+    const handleInputChange = (e) => {
+        dispatch({
+            type: ACTIONS.SET_VALUE,
+            field: e.target.name,
+            payload: e.target.value,
+        });
+    };
+
+    const handleSelectChange = (selectedOptions) => {
+        dispatch({
+            type: ACTIONS.SET_VALUE,
+            field: "amenities",
+            payload: selectedOptions.map((option) => option.value),
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(state);
+    }
+
     return (
-        <Form style={{ textAlign: "left", padding: "25px" }}>
+        <Form style={{ textAlign: "left", padding: "25px" }} onSubmit={handleSubmit}>
             <Form.Group>
                 <Row style={{ marginTop: "20px" }}>
-                    <Col>
-                        <Form.Group controlId="City">
-                            <Form.Label style={{ fontWeight: "bold" }}>
-                                City
-                            </Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter city"
-                            />
-                        </Form.Group>
-                    </Col>
                     <Col>
                         <Form.Group controlId="Country">
                             <Form.Label style={{ fontWeight: "bold" }}>
@@ -31,7 +76,25 @@ const SearchPlaceForm = () => {
                             </Form.Label>
                             <Form.Control
                                 type="text"
-                                placeholder="Enter country"
+                                placeholder="Enter Destination Country"
+                                name="country"
+                                value={country}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="City">
+                            <Form.Label style={{ fontWeight: "bold" }}>
+                                City
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter Destination City"
+                                name="city"
+                                value={city}
+                                onChange={handleInputChange}
                             />
                         </Form.Group>
                     </Col>
@@ -44,15 +107,27 @@ const SearchPlaceForm = () => {
                             <Form.Label style={{ fontWeight: "bold" }}>
                                 Minimum Price Per Night in $
                             </Form.Label>
-                            <Form.Control type="Number" value={0} min={0} />
+                            <Form.Control
+                                type="Number"
+                                name="minPrice"
+                                value={minPrice}
+                                min={0}
+                                onChange={handleInputChange}
+                            />
                         </Form.Group>
                     </Col>
                     <Col>
-                        <Form.Group controlId="minPrice">
+                        <Form.Group controlId="maxPrice">
                             <Form.Label style={{ fontWeight: "bold" }}>
                                 Maximum Price Per Night in $
                             </Form.Label>
-                            <Form.Control type="Number" value={0} min={0} />
+                            <Form.Control
+                                type="Number"
+                                name="maxPrice"
+                                value={maxPrice}
+                                min={0}
+                                onChange={handleInputChange}
+                            />
                         </Form.Group>
                     </Col>
                 </Row>
@@ -64,7 +139,13 @@ const SearchPlaceForm = () => {
                             <Form.Label style={{ fontWeight: "bold" }}>
                                 Minimum Number of Bedrooms
                             </Form.Label>
-                            <Form.Control type="Number" value={0} min={0} />
+                            <Form.Control
+                                type="Number"
+                                name="minCapacity"
+                                value={minCapacity}
+                                min={0}
+                                onChange={handleInputChange}
+                            />
                         </Form.Group>
                     </Col>
                     <Col>
@@ -74,9 +155,11 @@ const SearchPlaceForm = () => {
                             </Form.Label>
                             <Form.Control
                                 type="Number"
-                                value={0}
+                                name="minRating"
+                                value={minRating}
                                 min={0}
                                 max={5}
+                                onChange={handleInputChange}
                             />
                         </Form.Group>
                     </Col>
@@ -86,7 +169,12 @@ const SearchPlaceForm = () => {
                 <Form.Label style={{ fontWeight: "bold" }}>
                     Amenities
                 </Form.Label>
-                <Select isMulti name="amenities" options={amenitiesOptions} />
+                <Select
+                    isMulti
+                    name="amenities"
+                    options={amenitiesOptions}
+                    onChange={handleSelectChange}
+                />
             </Form.Group>
             <RegularBtn type="submit" style={{ marginTop: "20px" }}>
                 Search
