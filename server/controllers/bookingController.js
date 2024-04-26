bf3f980f8005f71c6606ff3612357797ac522e8a
Samanta Karmaker <samonto0726@gmail.com>
2024-04-26 22:14:46 +0600

    Added getBookingsByUserId controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -24,6 +24,35 @@ const createBooking = async (req, res, next) => {
     }
 }
 
+// Get bookings by user id
+const getBookingsByUserId = async (req, res, next) => {
+    try {
+        const userId = req.user.userId;
+        const bookings = await Bookings.find({ userId: userId });
+        if(bookings && bookings.length > 0) {
+            res.status(200).json({
+                success: true,
+                message: "Bookings found",
+                bookings: bookings,
+            });
+        }
+        else {
+            res.status(404).json({
+                success: false,
+                message: "No bookings found",
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+}
+
 module.exports = {
     createBooking,
+    getBookingsByUserId,
 };
