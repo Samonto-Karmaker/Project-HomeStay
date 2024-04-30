4665632bdcad6d143ab1fe3868e318a200fe4e32
Samanta Karmaker <samonto0726@gmail.com>
2024-04-30 22:34:20 +0600

    Changed get bookings by user id to include place details in bookings

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,6 @@
 // Internal imports
 const Bookings = require("../models/Bookings");
+const Places = require("../models/Places");
 
 //Create new booking
 const createBooking = async (req, res, next) => {
@@ -28,8 +29,21 @@ const createBooking = async (req, res, next) => {
 const getBookingsByUserId = async (req, res, next) => {
     try {
         const userId = req.user.userId;
-        const bookings = await Bookings.find({ userId: userId });
+        let bookings = await Bookings.find({ userId: userId });
         if(bookings && bookings.length > 0) {
+
+            // Promise.all() is used to wait for all the promises to resolve
+            bookings = await Promise.all(bookings.map(async (booking) => {
+                const place = await Places.findById(booking.placeId);
+
+                // Convert booking document to JavaScript object
+                booking = booking.toObject();
+                booking.placeName = place.name;
+                booking.placeLocation = place.city + ", " + place.country;
+                delete booking.placeId;
+                delete booking.userId;
+                return booking;
+            }))
             res.status(200).json({
                 success: true,
                 message: "Bookings found",
